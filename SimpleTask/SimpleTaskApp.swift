94321c9e0758210cb03c59c9982a80be8bbe32b8
Arturo Villalobos <rturohouse@gmail.com>
2023-11-23 10:02:44 -0600

    Initial Commit

---
+++ SimpleTask/SimpleTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTaskApp.swift
+//  SimpleTask
+//
+//  Created by Arturo  Villalobos on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
