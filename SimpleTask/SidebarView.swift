be2f8dcaab89f60ae5f4e5098278000ab41bf913
Arturo Villalobos <rturohouse@gmail.com>
2023-11-25 09:39:56 -0600

    i forgor what i did this commit was in the morning

--- SimpleTask/SidebarView.swift
+++ SimpleTask/SidebarView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct SidebarView: View {
     
     let userCreatedGroups: [TaskGroup]
-    
+    @State private var selection = TaskSection.all
     var body: some View {
-        List{
-            ForEach(TaskSection.allCases){ selection in
-                Label(selection.displayName,systemImage: selection.iconName)
+        List(selection: $selection ){
+            Section("Favorites"){
+                ForEach(TaskSection.allCases){ selection in
+                    Label(selection.displayName,systemImage: selection.iconName).tag(selection)
+                }
             }
-            ForEach(userCreatedGroups){ group in
-                Label(group.title, systemImage: "folder")
+            Section("Your Groups"){
+                ForEach(userCreatedGroups){ group in
+                    Label(group.title, systemImage: "folder").tag(TaskSection.list(group))
+                }
             }
         }
     }
